bbc78547acd616b4523de56e3af82d73841b702c
Espen Rønning <ronesp13@student.westerdals.no>
2015-04-25 10:32:13 +0200

    Added log4j

--- src/main/java/no/woact/pg4100/assignment3/ronesp13/controllers/PrimeController.java
+++ src/main/java/no/woact/pg4100/assignment3/ronesp13/controllers/PrimeController.java
@@ -1,6 +1,8 @@
 package no.woact.pg4100.assignment3.ronesp13.controllers;
 
 import no.woact.pg4100.assignment3.ronesp13.models.PrimeHandler;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,6 +14,8 @@ import java.io.IOException;
 @WebServlet("/prime-check")
 public class PrimeController extends HttpServlet {
 
+    private static final Logger logger = LogManager.getLogger(PrimeController.class.getName());
+
     private static final String PRIME_PAGE = "prime";
     private static final String NOT_PRIME_PAGE= "not_a_prime";
     private static final String INVALID_INPUT = "invalid_input";
@@ -20,6 +24,7 @@ public class PrimeController extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String address;
         String value = request.getParameter("prime");
+        logger.info("The input value " + value + " will be checked.");
         PrimeHandler prime = new PrimeHandler(value);
         if (!prime.isInteger()) {
             address = INVALID_INPUT;
--- src/main/java/no/woact/pg4100/assignment3/ronesp13/models/PrimeHandler.java
+++ src/main/java/no/woact/pg4100/assignment3/ronesp13/models/PrimeHandler.java
@@ -1,8 +1,13 @@
 package no.woact.pg4100.assignment3.ronesp13.models;
 
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.apache.commons.math3.primes.Primes;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class PrimeHandler {
+
+    private static final Logger logger = LogManager.getLogger(PrimeHandler.class.getName());
 
     private boolean isInteger;
     private boolean isPrime;
@@ -19,6 +24,7 @@ public class PrimeHandler {
             Integer.parseInt(value);
             return isInteger = true;
         } catch (NumberFormatException e) {
+            logger.error(ExceptionUtils.getStackTrace(e));
             return false;
         }
     }
